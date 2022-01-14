3656fbfa80f19d4388217542815a3772c0a5cd5e
Shane Lyse <mrshanelyse@hotmail.com>
2022-01-14 11:21:33 -0800

    added js button constants

--- js/calculator.js
+++ js/calculator.js
@@ -1,40 +1,7 @@
-let displayValue = 0;
-
-function add(x, y) {
-    return x + y;
-}
-
-function subtract(x, y) {
-    return x - y;
-}
-
-function multiply(x, y) {
-    return x * y;
-}
-
-function divide(x, y) {
-    if (y === 0) {
-        return "Illegal to divide by 0";
-    }
-
-    return x / y;
-}
-
-function operate(operator, x, y) {
-    switch (operator) {
-        case '+':
-            add(x, y);
-            break;
-        case '-':
-            subtract(x, y);
-            break;
-        case '*':
-            multiply(x, y);
-            break;
-        case '/':
-            divide(x, y);
-            break;
-        default:
-            return "Illegal operator not supported";
-    }
-}
+const numberButtons = document.querySelectorAll('[data-number]');
+const operationButtons = document.querySelectorAll('[data-operation]');
+const equalsButton = document.querySelector('[data-equals]');
+const deleteButton = document.querySelector('[data-delete]');
+const allClearButton = document.querySelector('[data-all-clear]');
+const previousOperandTextElement = document.querySelector('[data-previous-operand]');
+const currentOperandTextElement = document.querySelector('[data-current-operand]');
